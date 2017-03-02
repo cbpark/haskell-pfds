e00027168656d857a522574af681e21548b69e7f
cbpark <cbpark@gmail.com>
2017-03-02 18:14:01 +0900

    Add a default implementation of snoc for CatenableList

--- src/CatenableList.hs
+++ src/CatenableList.hs
@@ -1,11 +1,15 @@
 module CatenableList (CatenableList (..)) where
 
+import Prelude hiding ((++))
+
 class CatenableList c where
     empty   :: c a
     isEmpty :: c a -> Bool
 
     cons    :: a -> c a -> c a
     snoc    :: c a -> a -> c a
+    snoc xs x = xs ++ cons x empty
+
     (++)    :: c a -> c a -> c a
 
     -- head    :: c a -> a

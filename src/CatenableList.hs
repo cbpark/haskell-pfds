33477a354d3eefa77b9e6213c2f923d2a7e8f0e3
cbpark <cbpark@gmail.com>
2017-03-02 18:08:14 +0900

    Add CatenableList

---
+++ src/CatenableList.hs
@@ -0,0 +1,14 @@
+module CatenableList (CatenableList (..)) where
+
+class CatenableList c where
+    empty   :: c a
+    isEmpty :: c a -> Bool
+
+    cons    :: a -> c a -> c a
+    snoc    :: c a -> a -> c a
+    (++)    :: c a -> c a -> c a
+
+    -- head    :: c a -> a
+    head    :: c a -> Maybe a
+    -- tail    :: c a -> c a
+    tail    :: c a -> Maybe (c a)
